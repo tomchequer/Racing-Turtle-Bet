6c65c5138a2f05d564efd18de6930ae294c449a4
antonio <antoniochequer@gmail.com>
2021-07-28 16:24:11 -0300

    #added breaks for the text #improved the interpolation/concatenation

--- day 1/bandgenerator.py
+++ day 1/bandgenerator.py
@@ -3,8 +3,8 @@
 
 print('Welcome!This is a band name generator app.')
 
-city = input('Tell me, in which city or town were you born?')
+city = input('Tell me, in which city or town were you born?\n')
 
-petsname = input('Cool! What was your first pets name?')
+petsname = input('Cool! What was your first pets name?\n')
 
-print ("Why don't you call your band " + city + " " + petsname + "?")
+print(f"Why don't you call your band {city} {petsname} ?")
