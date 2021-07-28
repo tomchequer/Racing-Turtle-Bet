809d871c10febf4823f00d7fc46861c824ce9bc3
antonio <antoniochequer@gmail.com>
2021-07-28 15:20:50 -0300

    The first version of the project, without the best practices of interpolation yet!

---
+++ day 1/bandgenerator.py
@@ -0,0 +1,10 @@
+#This project will help you choose your band name!
+
+
+print('Welcome!This is a band name generator app.')
+
+city = input('Tell me, in which city or town were you born?')
+
+petsname = input('Cool! What was your first pets name?')
+
+print ("Why don't you call your band " + city + " " + petsname + "?")
