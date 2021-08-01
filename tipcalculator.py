f355d25bb81b6b58f1488e1df3d6a21adfccec33
antonio <antoniochequer@gmail.com>
2021-07-31 23:00:26 -0300

    Added a couple of words and created the logic on generating the words + generating the blank spaces.

---
+++ tipcalculator.py
@@ -0,0 +1,11 @@
+#This should be a simple program which should 
+# tell you how much youll have 
+#to pay for your bill, including the fact
+#that you'll split it with friends and tip 
+# the waiter some ammount.
+
+print('Welcome to the tip calculator!')
+value = float(input('How much is the final value of the bill?\n'))
+num_of_people = int(input('With how many people are you splitting the bill?\n'))
+tip_ammount = float(input ('How much, in terms of percentage do you wanna pay as a tip?\n'))
+
