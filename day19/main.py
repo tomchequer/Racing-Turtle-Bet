050596cf9941a8c1d4d584dc4d5be8ad35da77bd
antonio <antoniochequer@gmail.com>
2021-08-10 16:01:37 -0300

    started the project

---
+++ day19/main.py
@@ -0,0 +1,13 @@
+#Turtle Racing Bet game
+#A simple python game/simulation
+#Where you can choose a color, and 
+#see if your turtle will get lucky.
+#built using the turtle.py module.
+
+from turtle import Turtle, Screen
+
+screen = Screen()
+
+
+
+screen.exitonclick()
